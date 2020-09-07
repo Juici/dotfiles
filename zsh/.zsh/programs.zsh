58fcaf23dfdc5a9db6f9178047899adaede54de3
James Whaley <juicy66173@gmail.com>
2020-09-08 00:34:36 +0100

    zsh: Fix extract shenanigans with directories

--- zsh/.zsh/programs.zsh
+++ zsh/.zsh/programs.zsh
@@ -1,11 +1,11 @@
 # Replacement for ls.
-zinit ice wait'0' lucid if'(( ! $+commands[exa] ))' from'gh-r' as'program' mv'exa* -> exa'
+zinit ice wait'0' lucid if'(( ! $+commands[exa] ))' from'gh-r' as'program' mv'exa* -> exa' atclone'curl -o _exa https://raw.githubusercontent.com/ogham/exa/master/contrib/completions.zsh' atpull'%atclone'
 zinit light ogham/exa
 
 # Replacement for grep.
-zinit ice wait'0' lucid if'(( ! $+commands[rg] ))' from'gh-r' as'program' mv'complete/_rg -> _rg'
+zinit ice wait'0' lucid if'(( ! $+commands[rg] ))' from'gh-r' as'program' mv'*/rg -> rg'
 zinit light BurntSushi/ripgrep
 
 # Replacement for find.
-zinit ice wait'0' lucid if'(( ! $+commands[fd] ))' from'gh-r' as'program' mv'autocomplete/_fd -> _fd'
+zinit ice wait'0' lucid if'(( ! $+commands[fd] ))' from'gh-r' as'program' mv'*/fd -> fd'
 zinit light sharkdp/fd
