79e2beb4326f3cd1437e3e144de51d5956443be9
James Whaley <juicy66173@gmail.com>
2020-10-25 17:46:19 +0000

    zsh: Unset keychain async fd after finished usage

--- zsh/.zsh/plugins/keychain/keychain.plugin.zsh
+++ zsh/.zsh/plugins/keychain/keychain.plugin.zsh
@@ -5,10 +5,15 @@
 }
 
 _keychain_async_callback() {
+    # Eval keychain output.
     eval "$(<&$1)"
 
-    zle -F "$1"
+    # Close the file descriptor and the remove handler.
     exec {1}<&-
+    zle -F "$1"
+
+    # Unset the keychain async fd variable.
+    unset _keychain_async_fd
 }
 
 # If there is a pending task cancel it.
@@ -16,6 +21,9 @@ if [[ -n "$_keychain_async_fd" ]] && { true <&$_keychain_async_fd } 2>/dev/null;
     # Close the file descriptor and remove the handler.
     exec {_keychain_async_fd}<&-
     zle -F $_keychain_async_fd
+
+    # Unset the keychain async fd variable.
+    unset _keychain_async_fd
 fi
 
 # Fork a process to run keychain and open a pipe to read from it.
