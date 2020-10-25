07ee5a9cb284f31027c7f88866efdcf19a4b414d
James Whaley <juicy66173@gmail.com>
2020-10-25 17:15:25 +0000

    zsh: Use forked process to run keychain async

--- zsh/.zsh/plugins/keychain/keychain.plugin.zsh
+++ zsh/.zsh/plugins/keychain/keychain.plugin.zsh
@@ -4,5 +4,21 @@
     return 1
 }
 
-eval $(keychain --quiet --confhost --noask --eval id_rsa --agents gpg,ssh)
+_keychain_async_callback() {
+    eval "$(<&$1)"
+}
+
+# If there is a pending task cancel it.
+if [[ -n "$_keychain_async_fd" ]] && { true <&$_keychain_async_fd } 2>/dev/null; then
+    # Close the file descriptor and remove the handler.
+    exec {_keychain_async_fd}<&-
+    zle -F $_keychain_async_fd
+fi
+
+# Fork a process to run keychain and open a pipe to read from it.
+exec {_keychain_async_fd}< <(
+    keychain --quiet --confhost --noask --eval id_rsa --agents gpg,ssh
+)
+
+zle -F "$_keychain_async_fd" _keychain_async_callback
 
