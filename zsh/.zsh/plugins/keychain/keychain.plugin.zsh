cc4081138808f878e16ff853342d03b305cc3792
James Whaley <juicy66173@gmail.com>
2020-10-27 13:54:44 +0000

    zsh: Remove pointless async from keychain plugin
    The async in the keychain plugin is pointless, since it sits on top of
    the already async turbo-load from zinit. It just makes the reports from
    zinit less accurate.

--- zsh/.zsh/plugins/keychain/keychain.plugin.zsh
+++ zsh/.zsh/plugins/keychain/keychain.plugin.zsh
@@ -4,34 +4,4 @@
     return 1
 }
 
-_keychain_async_callback() {
-    # Eval keychain output.
-    eval "$(<&$1)"
-
-    # Close the file descriptor and the remove handler.
-    exec {1}<&-
-    zle -F "$1"
-
-    # Unset the keychain async fd variable.
-    unset _keychain_async_fd
-    # Remove the callback function.
-    unfunction _keychain_async_callback
-}
-
-# If there is a pending task cancel it.
-if [[ -n "$_keychain_async_fd" ]] && { true <&$_keychain_async_fd } 2>/dev/null; then
-    # Close the file descriptor and remove the handler.
-    exec {_keychain_async_fd}<&-
-    zle -F $_keychain_async_fd
-
-    # Unset the keychain async fd variable.
-    unset _keychain_async_fd
-fi
-
-# Fork a process to run keychain and open a pipe to read from it.
-exec {_keychain_async_fd}< <(
-    keychain --quiet --confhost --noask --eval id_rsa --agents gpg,ssh
-)
-
-zle -F "$_keychain_async_fd" _keychain_async_callback
-
+eval "$(keychain --quiet --confhost --noask --eval id_rsa --agents gpg,ssh)"
