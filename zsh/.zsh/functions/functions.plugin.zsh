6e83ffdbcee9207bd8e5ca0220c1a9960e061bf1
James Whaley <juicy66173@gmail.com>
2020-11-08 19:10:18 +0000

    zsh: Add jump function for faster navigation
    Add jump function for faster navigation to common directories.
    
    The `~/.jump_points` file is used to configure the jump points; it accepts
    plain strings or glob patterns for specifying jump points. Comments can
    be added using the `#` charater for line comments.

--- zsh/.zsh/functions/functions.plugin.zsh
+++ zsh/.zsh/functions/functions.plugin.zsh
@@ -16,7 +16,9 @@ if [[ ${zsh_loaded_plugins[-1]} != */functions && -z ${fpath[(r)${0:h}]} ]] {
 typeset -gA Plugins
 Plugins[FUNCTIONS_DIR]="${0:h}"
 
-autoload -Uz bar tw parseoffset
+autoload -Uz bar tw parseoffset jump
+
+alias j='jump'
 
 # Use alternate vim marks [[[ and ]]] as the original ones can
 # confuse nested substitutions, e.g.: ${${${VAR}}}
