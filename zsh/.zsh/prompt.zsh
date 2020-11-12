5b07523a8c644e22f95f35d66343a6db9ab7c0ec
James Whaley <juicy66173@gmail.com>
2020-11-12 16:13:33 +0000

    zsh: Fix prompt issues when gitstatus crashes
    Add fallback to vcs_info if gitstatus keeps crashing.

--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -8,7 +8,7 @@ autoload -Uz colors
 colors
 
 # Load gitstatus plugin and start daemon.
-zinit ice wait nocd lucid atload'gitstatus_stop "gitprompt" && gitstatus_start "gitprompt" && .prompt_async_tasks'
+zinit ice wait nocd lucid atload'gitstatus_stop "gitprompt" && gitstatus_start "gitprompt"; .prompt_async_tasks'
 zinit load romkatv/gitstatus
 
 # http://zsh.sourceforge.net/Doc/Release/User-Contributions.html#Version-Control-Information
@@ -19,18 +19,51 @@ zstyle ':vcs_info:*' get-revision true
 zstyle ':vcs_info:*' stagedstr '1' # Boolean value.
 zstyle ':vcs_info:*' unstagedstr '1' # Boolean value.
 zstyle ':vcs_info:*' use-simple true
-zstyle ':vcs_info:*' max-exports 7
+zstyle ':vcs_info:*' max-exports 8
+
+# Hook debug info.
+zstyle ':vcs_info:*+*:*' debug false
 
 # VCS format strings.
+#
 # %R - top repo directory
 # %b - branch
 # %i - revision
-# %a - action
 # %m - misc
-# %c - stagedstr
-# %u - unstagedstr
-zstyle ':vcs_info:*:*' formats '%R' '%b' '%i' '%m' '%c' '%u'
-zstyle ':vcs_info:*:*' actionformats '%R' '%b' '%i' '%m' '%c' '%u' '%a'
+# %c - has staged
+# %u - has unstaged
+# 0  - has untracked
+# %a - action
+#
+# By default vcs_info doesn't support untracked files, so we pretend that there
+# are none and we will override it with a hook.
+zstyle ':vcs_info:*:*' formats '%R' '%b' '%i' '%m' '%c' '%u' '0'
+zstyle ':vcs_info:*:*' actionformats '%R' '%b' '%i' '%m' '%c' '%u' '0' '%a'
+
+zstyle ':vcs_info:git+set-message:*' hooks git-untracked
+
++vi-git-untracked() {
+    emulate -L zsh
+    setopt extended_glob warn_create_global typeset_silent no_short_loops rc_quotes no_auto_pushd
+
+    integer n=$1
+
+    # `vcs_info_msg_6_` carries the 'has untracked' state, it defaults to `0`.
+    if (( n == 6 )); then
+        # Untracked files are marked by lines starting '??', so we filter on
+        # those lines.
+        local -a untracked
+        untracked=( ${(@M)${(@f)"$(git status --porcelain --ignore-submodules=all --untracked-files=all 2>/dev/null)"}##'??'*} )
+
+        # If there are untracked files update the message.
+        if (( ${#untracked} > 0 )); then
+            # Override the message.
+            hook_com[message]='1'
+            # Set `ret` to non-zero so that our message is used.
+            ret=1
+        fi
+    fi
+}
 
 # }}}
 
@@ -246,14 +279,22 @@ typeset -gF EPOCHREALTIME
     if [[ -d "$1" ]]; then
         builtin cd -q $1
     else
+        # The path is not an existing directory, it may have been removed.
         return 1
     fi
 
+    # The number of times the gitstatus daemon has crashed this prompt.
+    integer gitstatus_crashes=$2
+
     local -A info
 
-    # Execute gitstatus query if function loaded.
-    if command -v gitstatus_query >/dev/null; then
-        gitstatus_query 'gitprompt'
+    # Execute gitstatus query if function loaded and gitstatus is not disabled.
+    if (( gitstatus_crashes != -1 )) && command -v gitstatus_query >/dev/null; then
+        # Run the query, if it exits with a failure status the daemon has
+        # crashed and needs to be restarted.
+        if ! gitstatus_query 'gitprompt' 2>/dev/null; then
+            export VCS_STATUS_RESULT='crashed'
+        fi
     else
         export VCS_STATUS_RESULT='norepo-sync'
     fi
@@ -276,21 +317,26 @@ typeset -gF EPOCHREALTIME
             info[misc]=''
 
             info[staged]=$(( VCS_STATUS_HAS_STAGED ))
-            info[unstaged]=$(( VCS_STATUS_HAS_UNSTAGED == 1 )) # Ignore unknown = -1.
-            info[untracked]=$(( VCS_STATUS_HAS_UNTRACKED == 1 )) # Ignore unknown = -1.
+            info[unstaged]=$(( VCS_STATUS_HAS_UNSTAGED == 1 )) # Ignore unknown (-1).
+            info[untracked]=$(( VCS_STATUS_HAS_UNTRACKED == 1 )) # Ignore unknown (-1).
             ;;
         norepo-sync)
             # Check other VCS.
-            vcs_info
+            vcs_info >&2
 
             info[root]="$vcs_info_msg_0_"
             info[branch]="$vcs_info_msg_1_"
+
             info[revision]="$vcs_info_msg_2_"
-            info[action]="$vcs_info_msg_6_"
+            info[action]="$vcs_info_msg_7_"
             info[misc]="$vcs_info_msg_3_"
+
             info[staged]=$(( vcs_info_msg_4_ ))
             info[unstaged]=$(( vcs_info_msg_5_ ))
-            info[untracked]=0
+            info[untracked]=$(( vcs_info_msg_6_ ))
+            ;;
+        crashed)
+            info[gitstatus_crashes]=$(( gitstatus_crashes + 1 ))
             ;;
     esac
 
@@ -303,6 +349,13 @@ typeset -gF EPOCHREALTIME
     emulate -L zsh
     setopt extended_glob warn_create_global typeset_silent no_short_loops rc_quotes no_auto_pushd
 
+    # The number of times the gitstatus daemon has crashed this prompt.
+    # If gitstatus is disabled this value should be -1.
+    #
+    # This is to prevent an infinite cycle of restarting the daemon only for it
+    # to crash.
+    integer gitstatus_crashes=${${Prompt[gitstatus_disabled]:+-1}:-$1}
+
     # Note: exec doesn't accept variables in the form of associative arrays, so
     #       we have to go through a intermediate variable 'async_fd'.
     integer async_fd
@@ -312,7 +365,6 @@ typeset -gF EPOCHREALTIME
         # Close the file descriptor.
         async_fd=${Prompt[async_fd]}
         exec {async_fd}<&-
-        unset async_fd
 
         # Remove the handler.
         zle -F ${Prompt[async_fd]}
@@ -327,10 +379,9 @@ typeset -gF EPOCHREALTIME
     # Fork a process to fetch VCS info and open a pipe to read from it.
     exec {async_fd}< <(
         # Fetch and print VCS info.
-        .prompt_async_vcs_info "$PWD"
+        .prompt_async_vcs_info "$PWD" $gitstatus_crashes
     )
     Prompt[async_fd]=$async_fd
-    unset async_fd
 
     # When the file descriptor is readable, run the callback handler.
     zle -F "${Prompt[async_fd]}" →prompt_async_callback
@@ -338,9 +389,9 @@ typeset -gF EPOCHREALTIME
 
 →prompt_async_callback() {
     emulate -L zsh
-    setopt extended_glob warn_create_global typeset_silent no_short_loops rc_quotes no_auto_pushd
+    setopt extended_glob warn_create_global typeset_silent no_short_loops rc_quotes no_auto_pushd no_prompt_vars
 
-    local async_fd=$1
+    integer async_fd=$1
 
     # Read from file descriptor.
     local read_in="$(<&$async_fd)"
@@ -356,6 +407,47 @@ typeset -gF EPOCHREALTIME
 
     # Parse output (z) and unquote as array (Q@).
     info=( "${(Q@)${(z)read_in}}" )
+
+    integer gitstatus_crashes=${info[gitstatus_crashes]}
+
+    # The gitstatus daemon has crashed and needs restarting.
+    if (( gitstatus_crashes > 0 )); then
+        integer gitstatus_crash_threshold=3
+
+        # If the daemon has crashed a threshold number of times this prompt,
+        # there is probably an underlying issue.
+        if (( gitstatus_crashes >= gitstatus_crash_threshold )); then
+            # If in ZLE move to first column and clear line. The prompt will be
+            # reset when the callback chain finishes.
+            #
+            # This prevents the message from getting garbled.
+            zle && print -n "${terminfo[cr]}${terminfo[el]}"
+
+            # Notify the user of the crashes.
+            print -P "%F{red}prompt%f: gitstatus daemon crashed %F{yellow}${gitstatus_crashes}%f times in one prompt cycle"
+            print -P '%F{blue}prompt%f: switching to %F{green}vcs_info%f backup'
+
+            # Disable gitstatus for the session.
+            Prompt[gitstatus_disabled]=1
+
+            # Retrieve enabled vcs_info backends.
+            local -aU vi_enabled_backends
+            zstyle -a ':vcs_info:*' enable vi_enabled_backends
+
+            # Add git to array of enabled vcs_info backends.
+            vi_enabled_backends+=( git )
+            zstyle ':vcs_info:*' enable $vi_enabled_backends
+        else
+            # Restart the gitstatus daemon.
+            gitstatus_stop 'gitprompt' 2>/dev/null
+            gitstatus_start 'gitprompt'
+        fi
+
+        # Rerun the async prompt tasks, keeping track of number of crashes.
+        .prompt_async_tasks $gitstatus_crashes
+
+        return
+    fi
 
     # Check if the path has changed since the job started, if so abort.
     [[ "${info[pwd]}" != "$PWD" ]] && return
