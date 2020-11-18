fe246616083c5b19def0d9208ff12fedbf6e938f
James Whaley <juicy66173@gmail.com>
2020-11-18 22:24:07 +0000

    kitty: Clean up dropdown terminal script
    Use zsh for dropdown terminal script.
    Split the script into modular functions.
    Add `--show`, `--hide`, `--toggle` flags.

--- kitty/.local/share/tdrop-kitty/watcher.py
+++ kitty/.local/share/tdrop-kitty/watcher.py
@@ -1,9 +1,18 @@
+import os
+import subprocess
+
+
 def on_resize(boss, window, data):
     pass
 
 
 def on_focus_change(boss, window, data):
-    pass
+    focused = data['focused']
+
+    # If the window is unfocused hide the dropdown.
+    if not focused:
+        tdrop_kitty = os.path.expanduser('~/.local/bin/tdrop-kitty')
+        subprocess.run([tdrop_kitty, '--hide'])
 
 
 def on_close(boss, window, data):
