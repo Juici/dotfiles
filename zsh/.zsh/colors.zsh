67d943a9e099c2164d98be575df328d95fb9d83a
James Whaley <juicy66173@gmail.com>
2022-02-20 14:03:59 +0000

    zsh: Adjust zstyle completion colours

--- zsh/.zsh/colors.zsh
+++ zsh/.zsh/colors.zsh
@@ -6,5 +6,5 @@ zi light-mode for \
     pick'clrs.zsh' \
     atclone'dircolors -b LS_COLORS >! clrs.zsh' \
     atpull'%atclone' \
-    atload'zstyle ":completion:*" list-colors "${(s.:.)LS_COLORS}"' \
+    atload'zstyle ":completion:*:default" list-colors "${(s.:.)LS_COLORS}"' \
         trapd00r/LS_COLORS
